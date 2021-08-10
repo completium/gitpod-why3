bfe2d4f948b1362b53e916a47a261fde4faba27a
Benoît Rognier <benoit.rognier@edulera.com>
2021-08-10 17:14:40 +0200

    adds install

---
+++ install.sh
@@ -0,0 +1,14 @@
+sudo apt update
+sudo apt install why3 -y
+sudo apt install alt-ergo -y
+rm -f z3-4.8.6-x64-ubuntu-16.04.zip
+wget https://github.com/Z3Prover/z3/releases/download/z3-4.8.6/z3-4.8.6-x64-ubuntu-16.04.zip
+unzip -fo z3-4.8.6-x64-ubuntu-16.04.zip
+cd z3-4.8.6-x64-ubuntu-16.04
+cd bin
+sudo cp z3 /usr/bin
+why3 config --detect
+sudo apt install yaru-* -y
+sudo mkdir /home/gitpod/.config/gtk-3.0
+sudo cp -f ./settings/setting.ini /home/gitpod/.config/gtk-3.0/settings.ini
+sudo cp ./settings/.why3.conf /home/gitpod/.why3.conf
